cfd65213c7269d5bf99a750652a7b225d85b7dce
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-22 15:41:01 +0100

    feat: create Cane class w/ methods

---
+++ Java Abstract/src/org/abstracts/animals/Cane.java
@@ -0,0 +1,23 @@
+package org.abstracts.animals;
+
+public class Cane extends Animale {
+
+	@Override
+	public void dormi() {
+		System.out.println("zzz");
+
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("wof");
+
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("cibo per cani");
+
+	}
+
+}
