b6ee21485c5471f6575f41e795aa4cb2a2238c72
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-22 16:53:03 +0100

    feat: create Aquila class w/ methods

---
+++ Java Abstract/src/org/abstracts/animals/Aquila.java
@@ -0,0 +1,22 @@
+package org.abstracts.animals;
+
+public class Aquila extends Animale {
+
+	@Override
+	public void dormi() {
+		System.out.println("zzz");
+
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("aaah");
+
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("conigli");
+	}
+
+}
