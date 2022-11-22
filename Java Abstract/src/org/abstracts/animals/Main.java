191d92b3cedddab35c2f9c8c7cf7e9c9e87df432
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-22 17:21:07 +0100

    feat(Main): add test animal classes

--- Java Abstract/src/org/abstracts/animals/Main.java
+++ Java Abstract/src/org/abstracts/animals/Main.java
@@ -4,7 +4,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Cane c = new Cane("Cane");
+		System.out.println("Animale: " + c.getNome());
+		c.dormi();
+		c.mangia();
+		c.verso();
 		
+		System.out.println("\n----------\n");
+		
+		Passerotto p = new Passerotto("Passerotto");
+		System.out.println("Animale: " + p.getNome());
+		p.dormi();
+		p.mangia();
+		p.verso();
+		
+		System.out.println("\n----------\n");
+		
+		Aquila a = new Aquila("Aquila");
+		System.out.println("Animale: " + a.getNome());
+		a.dormi();
+		a.mangia();
+		a.verso();
+		
+		System.out.println("\n----------\n");
+		
+		Delfino d = new Delfino("Delfino");
+		System.out.println("Animale: " + d.getNome());
+		d.dormi();
+		d.mangia();
+		d.verso();
 	}
 
 }
